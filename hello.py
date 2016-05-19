f757e980e224aa88b30403727ec6d5e2a365b8cf
Miriam Rathbun <miriamrathbun13@gmail.com>
2016-05-19 14:35:23 -0400

    Add files via upload
    I just played around to practice some simple commands. Just want to see how uploading files/submitting changes works.

--- hello.py
+++ hello.py
@@ -1,3 +1,29 @@
 import sys
+#first time use: sudo apt-get install python-numpy 
+import numpy as np
 
 print('Hello, World! (python)')
+m=0
+i=0
+j=9
+print(m)
+
+if m==0:
+   n=2
+else:
+   n=1
+
+print(n)
+
+while i==0:
+	print(m)
+	m=m+1
+	if m==5:
+		i=i+5
+
+for j in range (0,3):
+	print('hi')
+
+print(np.cos(np.pi/6))
+print(np.sqrt(3)/2)
+

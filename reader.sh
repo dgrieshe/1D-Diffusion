13eaea56a2c33ce58be61c38fc51c506b40d3eea
Miriam Rathbun <miriamrathbun13@gmail.com>
2016-05-25 23:07:45 -0400

    Add files via upload

---
+++ reader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#not actually sure what this first line does
+#last edited by Miriam Rathbun on 5/25/2016
+#script reads an input file of known structure. It loops through all .inp files in the folder and runs the python script for each one of those. 
+
+for f in `ls *.inp`
+do
+   name=$f							#finds input files in the folder
+   echo $name
+
+   length=`awk '/length/{print $2}' $name` 					#finds the line where the word "length" appears, and prints column 2 of that line
+   rightBC=`awk '/rightBC/{print $2}' $name` 
+   leftBC=`awk '/leftBC/{print $2}' $name`
+   numgroups=`awk '/numgroups/{print $2}' $name`
+
+
+python ./vector_practice.py $length $rightBC $leftBC $numgroups $f
+done
--- vector_practice.py
+++ vector_practice.py
@@ -1,11 +1,24 @@
-#last edited by Miriam Rathbun on 5/19/2016
-#This file solves for x such that Ax=B after generating a random square matrix A (nxn) and vector B (length n)
+#last edited by Miriam Rathbun on 5/25/2016
+#this script is run from reader.sh. It creates two matrices A and B and solves for x such that Ax=B. Output is dumped to files in the output folder. 
+
+#use sudo apt-get install python.numpy if needed for any of these (required the first time on a new machine)
 import numpy as np
 import random
+import sys, os
+import test
+import matplotlib.pyplot as plt
+
+#names the variables that came in from the input file
+length= int(sys.argv[1])
+rightBC = sys.argv[2]
+leftBC = sys.argv[3]
+numgroups = int(sys.argv[4])
+f = sys.argv[5]											#name of the input file
+name = f[len(f)-5]										#retains only the number associated with the input file. Ex: "input1" becomes "1." This was made so the ouput could have a pretty name 
 
 #define a square matrix A
 #note: counting starts at 0 for rows and cols
-n=random.randint(2,10)
+n=length  												#just to show that the passed variables can be used in this script
 A=np.zeros((n,n)) 										#creates a random square matrix A (dimensions (n,n)) for the Ax=B calc
 B=np.zeros((n,1))										#creates vector B of length n
 
@@ -21,18 +34,16 @@ while det==0:											#if det=0, A is singular and should be re-made
 
 Ainv=np.linalg.inv(A)									#take the inverse of A
 x=np.dot(Ainv,B)										#solution x= matrix multiplication of Ainv and B
-print('Solution to Ax=B where A=') 
-print(A)
-print('and B=')
-print(B)
-print('is:')
-print(x)
+
+
+f = open('./output/output_'+name+'.text', 'w')
+print >> f, 'Solution to Ax=B where A= \n', A
+print >> f, 'and B= \n', B
+print >> f, 'is: \n', x
+f.close()
 
 #print(B)
 #print(np.dot(A,x))										#check that A*x=B
-
-
-	   
 
 
 

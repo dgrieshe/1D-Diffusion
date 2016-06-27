28ea91c7855ed8bc86289fdf8f9b2662b8c8066f
Dave <dgrieshe@users.noreply.github.com>
2016-06-27 00:31:02 -0400

    Forgot to include the changes to hello.py in the last commit.

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 import sys
 #first time use: sudo apt-get install python-numpy 
 import numpy as np
+from diffusionOpts import *
+from nuclide import *
 
 print('Hello, World! (python)')
 m=0
@@ -20,4 +22,40 @@ for j in range (0,3):
 
 print(np.cos(np.pi/6))
 print(np.sqrt(3)/2)
+
+
+###########################################################################
+#
+# Simple example.  Create a nuclide 'A' with name U235, then read in the
+# xs data and print out the parameters.
+
+A = Nuclide('U235')
+A.read('input_filename')
+print(A.name)
+print(A.totalXs)
+
+# Here is a slightly more complex example where we add a Nuclide object
+# to a dictionary hashed by the nuclide name (e.g., the array key is the
+# nuclide name.  Note that we can access all of the methods attached to
+# the Nuclide object directly from the dictionary entry.
+
+nuclides = {}
+thisName = 'U238'
+nuclides[thisName] = Nuclide(thisName)
+nuclides[thisName].read('dummyFilename.txt')
+
+print(nuclides['U238'].name)
+print(nuclides['U238'].totalXs)
+
+###########################################################################
+#
+# Example to read in diffusion options.
+
+
+solverOptions = DiffusionOptions1D()
+solverOptions.read('input1.inp')
+print('Length:', solverOptions.length)
+print('Groups:', solverOptions.numGroups)
+
+
 

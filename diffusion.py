9045f39b21b3bdaf31ee1cb5e252ea8353d4bb38
Dave <dgrieshe@users.noreply.github.com>
2016-08-01 01:29:17 -0400

    Added new algorithm for constructing coefficient matrix.  Also fixed several small issues with original coding: 1) absorption rather than total cross section was being used in the removal term, 2) within-group inscattering source was not being added to the diagonial elements in the coefficient matrix.

--- diffusion.py
+++ diffusion.py
@@ -39,7 +39,7 @@ for f in fn.listfn:
 		M=Nuclide('U235(%i)' %k)
 		M.read()
 		for i in range(0,options.numBins):
-			xs.append(M.absXs)
+			xs.append(M.totalXs)
 			diff_coef.append(1/(3*(M.totalXs-u_g*M.scatXs)))
 		for j in range(0,options.numGroups):
 			scat[j,k-1]=eval('M.scatXs%i' %(j+1))
@@ -66,31 +66,127 @@ for f in fn.listfn:
 ###############################################################################
 #Filling matrices A (linear system of diffusion equations) and B (source)
 
-	
-	A=np.zeros((options.numBins*options.numGroups,options.numBins*options.numGroups))
-	B=np.zeros((options.numBins*options.numGroups,1))
-	
+#        # Alternative algorithm for constructing the linear system of equations.
+#        # This algorithm offers several potential advantages:
+#        #    - Simpler loop indexing scheme
+#        #    - Avoids looping over zero-value matrix entries
+#        #    - Avoids separate spatial loop for inscattering source
+#        #    - Can be used with sparse matrix representation
+#        #    - Easy to switch between Marshak and zero flux boundary conditions
+#        #    - Source code fits in 120 columns
+#        #    - Easy to convert cross section representation, which is important if
+#        #      we are computing cross sections on-the-fly.
+#
+#        nGrps = options.numGroups
+#        nBins = options.numBins
+#        rank  = nBins*nGrps
+#
+#        A = np.zeros((rank, rank))
+#        B = np.zeros((rank,1))
+#
+#        for g in range(0, nGrps):
+#           for x in range(0, nBins):
+#
+#              # Determine the unique phase index (cell and energy group) for this entry.
+#              # This value is used to index into the cross section arrays.
+#
+#              i = g*nBins+x
+#
+#              # Calculate the average diffusion theory between the current cell and the
+#              # adjacent cell to the left.  Note that we set coefficients consistent with
+#              # the Marshak boundary condition for the cells on the left boundary.  For a
+#              # zero flux boundary condition, set deltaAdj equal to zero instead of 4 here.
+#
+#              if x == 0:
+#                 dCoefAdj = 1
+#                 deltaAdj = 4     ## Marshak escape boundary condition
+#                 #deltaAdj = 0    ## Zero flux boundary condition
+#              else:
+#                 dCoefAdj = diff_coef[i-1]
+#                 deltaAdj = options.delta
+#
+#              dLeft = (2*diff_coef[i]*dCoefAdj)/(deltaAdj*diff_coef[i] + options.delta*dCoefAdj)
+#
+#              # Calculate the average diffusion theory between the current cell and the
+#              # adjacent cell to the right.  Note that we set coefficients consistent with
+#              # the Marshak boundary condition for the cells on the right boundary.  For a
+#              # zero flux boundary condition, set deltaAdj equal to zero instead of 4 here.
+#
+#              if x == (nBins-1):
+#                 dCoefAdj = 1
+#                 deltaAdj = 4     ## Marshak escape boundary condition
+#                 #deltaAdj = 0    ## Zero flux boundary condition
+#              else:
+#                 dCoefAdj = diff_coef[i+1]
+#                 deltaAdj = options.delta
+#
+#              dRight = (2*diff_coef[i]*dCoefAdj)/(deltaAdj*diff_coef[i] + options.delta*dCoefAdj)
+#
+#              # Set the matrix coefficient for the cell.  Note that this does not include the
+#              # within-group inscattering source term, as this will be included later.
+#
+#              A[i,i] = dLeft+dRight+xs[i]*options.delta
+#              #print(i, i, A[i,i])
+#
+#              # Set the matrix coefficients for the adjacent cells, except when processing a 
+#              # boundary cell.
+#
+#              if not x == 0:
+#                 A[i,i-1] = -dLeft
+#                 #print(i, i-1, A[i,i-1])
+#
+#              if not x == (nBins-1):
+#                 A[i,i+1] = -dRight
+#                 #print(i, i+1, A[i,i+1])
+#
+#              # Here we include the group-to-group inscattering source terms.  Because we are
+#              # dealing with small 1D systems these terms have been included in the coefficient matrix
+#              # rather than lagged in the source term, thus eliminating the need for an iteration
+#              # (or sweep) over the inscattering term.
+#
+#              # We have assumed no upscattering here, but this is not a requirement.  Simply
+#              # change the range of the incident neutron groups to include upscattering. 
+#
+#              for gIn in range(0, g+1):
+#                 iIn = gIn*nBins+x
+#                 A[i,iIn]=A[i,iIn]-scat[g,gIn]
+#                 #print(i, iIn, A[i,iIn])
+#
+#              # Finally we set the source term for the cell/energy group.
+#
+#              B[i,0]=source[i]*options.delta
 
-	for k in range(1,options.numGroups+1):
-		for row in range(options.numBins*(k-1),options.numBins*k):
-			for col in range(options.numBins*(k-1),options.numBins*k):
-				if row == col:
-					if row == options.numBins*(k-1):
-						A[row,col]=2*diff_coef[row]/(options.delta*1+4*diff_coef[row])+xs[row]*options.delta #+2*diff_coef[row]*diff_coef[row+1]/(options.delta*diff_coef[row+1]+options.delta*diff_coef[row])
-					elif row == options.numBins*k-1:
-						A[row,col]=2*diff_coef[row]/(options.delta*1+4*diff_coef[row])+xs[row]*options.delta #+2*diff_coef[row-1]*diff_coef[row]/(options.delta*diff_coef[row]+options.delta*diff_coef[row-1])
-					else:
-						A[row,col]=2*diff_coef[row]*diff_coef[row+1]/(options.delta*diff_coef[row+1]+options.delta*diff_coef[row])+2*diff_coef[row-1]*diff_coef[row]/(options.delta*diff_coef[row]+options.delta*diff_coef[row-1])+xs[row]*options.delta
-				elif col == row-1:
-					A[row,col]=-2*diff_coef[row-1]*diff_coef[row]/(options.delta*diff_coef[row]+options.delta*diff_coef[row-1])
-				elif col == row+1:
-					A[row,col]=-2*diff_coef[col-1]*diff_coef[col]/(options.delta*diff_coef[col]+options.delta*diff_coef[col-1]) 
-		for row in range(options.numBins,options.numBins*options.numGroups):
-			for col in range(0,options.numBins*options.numGroups):
-				if row == col+options.numBins*k:
-					A[row,col]=-scat[k,k-1]
-	for row in range(0,len(source)):
-		B[row,0]=source[row]*options.delta
+        # Original algorithm for constructing the linear system of equations.
+
+        A=np.zeros((options.numBins*options.numGroups,options.numBins*options.numGroups))
+        B=np.zeros((options.numBins*options.numGroups,1))
+
+        for k in range(1,options.numGroups+1):
+                for row in range(options.numBins*(k-1),options.numBins*k):
+                        for col in range(options.numBins*(k-1),options.numBins*k):
+                                if row == col:
+                                        if row == options.numBins*(k-1):
+                                                A[row,col]=2*diff_coef[row]/(options.delta*1+4*diff_coef[row])+xs[row]*options.delta+2*diff_coef[row]*diff_coef[row+1]/(options.delta*diff_coef[row+1]+options.delta*diff_coef[row])-scat[k-1,k-1]
+                                                #print(row, col, A[row,col])
+                                        elif row == options.numBins*k-1:
+                                                A[row,col]=2*diff_coef[row]/(options.delta*1+4*diff_coef[row])+xs[row]*options.delta+2*diff_coef[row-1]*diff_coef[row]/(options.delta*diff_coef[row]+options.delta*diff_coef[row-1])-scat[k-1,k-1]
+                                                #print(row, col, A[row,col])
+                                        else:
+                                                A[row,col]=2*diff_coef[row]*diff_coef[row+1]/(options.delta*diff_coef[row+1]+options.delta*diff_coef[row])+2*diff_coef[row-1]*diff_coef[row]/(options.delta*diff_coef[row]+options.delta*diff_coef[row-1])+xs[row]*options.delta-scat[k-1,k-1]
+                                                #print(row, col, A[row,col])
+                                elif col == row-1:
+                                        A[row,col]=-2*diff_coef[row-1]*diff_coef[row]/(options.delta*diff_coef[row]+options.delta*diff_coef[row-1])
+                                        #print(row, col, A[row,col])
+                                elif col == row+1:
+                                        A[row,col]=-2*diff_coef[col-1]*diff_coef[col]/(options.delta*diff_coef[col]+options.delta*diff_coef[col-1]) 
+                                        #print(row, col, A[row,col])
+                for row in range(options.numBins,options.numBins*options.numGroups):
+                        for col in range(0,options.numBins*options.numGroups):
+                                if row == col+options.numBins*k:
+                                        A[row,col]=-scat[k,k-1]
+                                        #print(row, col, A[row,col])
+        for row in range(0,len(source)):
+                B[row,0]=source[row]*options.delta
 
 ###############################################################################   
 
@@ -107,6 +203,3 @@ for f in fn.listfn:
 	results.plot(x,1,options.numBins,options.numGroups,name)
     
 ###############################################################################
-    
-    
-               

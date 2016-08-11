4f9b4518da021ad07790675adaaf2b03bfda96c0
Miriam Rathbun <miriamrathbun13@gmail.com>
2016-08-11 18:54:47 -0400

    updates
    The construction of matrix A  and Ainv is in a subroutine called Construct with functions constructA and invertA. I moved your notes about your alternative A construction algorithm to Construct.py.
    Solving the problem occurs in subroutine Solver with function solve.
    
    The whole Construct and Solve routines are called n times, each time dividing the cross sections in half.

--- diffusion.py
+++ diffusion.py
@@ -29,8 +29,11 @@ for f in fn.listfn:
 	options.read(f)
 	
 	#cross sections for each bin.
-	xs=[]
+	total_xs=[]
+	scat_xs=[]
 	diff_coef=[]
+	#diff_coef=np.zeros(options.numBins*options.numGroups)
+	#print len(diff_coef)
 	source=np.zeros(options.numBins*options.numGroups)
 	scat=np.zeros((options.numGroups,options.numGroups))
 	sourceGroup=[]
@@ -41,17 +44,17 @@ for f in fn.listfn:
 	#loops through the groups
 	#this section also fills other items which depend on groups, see below
 	for k in range(1,options.numGroups+1):
-		M= ('M%i' %k)
 		M=Nuclide('U235(%i)' %k)
-		M.read()                    
+		M.read()
 		for i in range(0,options.numBins):
-			xs.append(M.totalXs)
-			diff_coef.append(1/(3*(M.totalXs-u_g*M.scatXs)))
+			total_xs.append(M.totalXs)
+			scat_xs.append(M.scatXs)
+		for i in range(0,options.numBins):
+			diff_coef.append(1/(3*(total_xs[i]-u_g*scat_xs[i])))
 			
 		#fills the transition matrix/scattering kernel
 		for j in range(0,options.numGroups):  
-			scat[j,k-1]=eval('M.scatXs%i' %(j+1))
-			
+			scat[j,k-1]=eval('M.scatXs%i' %(j+1))  
 			
 ###############################################################################
 
@@ -70,23 +73,37 @@ for f in fn.listfn:
     	
 	
 ###############################################################################
-#Builds the linear system of equations
-			
-	A=Construct()
-	A.constructA(options, diff_coef, scat, xs)
-	
-#sets the initial value of the source
-	for i in range (0,len(source)):
-		source[i]=1/options.length
+
+
+	n=0
+	while n<2:
+		
+		if n !=0:
+			for i in range(0,len(total_xs)):
+				total_xs[i]=total_xs[i]/2
+				scat_xs[i]=scat_xs[i]/2
+				diff_coef[i]=diff_coef[i]/2
+			for k in range(1,options.numGroups+1):
+				for j in range(0,options.numGroups):
+					scat[j,k-1]=scat[j,k-1]/2
+		n=n+1
+		
+		#Builds the linear system of equations
+		A=Construct()
+		A.constructA(options, diff_coef, scat, total_xs)
 		
 		
+		#sets the initial value of the source
+		for i in range (0,len(source)):
+			source[i]=1/options.length
+
 ###############################################################################
 #calculates the solution x to Ax=B 	
-	
-	
-	A.invertA()
-	sol=Solve()
-	sol.solve(options, A.inv, source)
+
+
+		A.invertA()
+		sol=Solve()
+		sol.solve(options, A.inv, source)
 	
 	results=Plotter()
 	results.plot(sol.x,1,options.numBins,options.numGroups,name)
